57f50ae2a3b31dfca6e3383e438614e4a5fae655
Chloé <chloe.soquet@hotmail.com>
2021-12-01 11:07:16 +0100

    Ajout spring security basic

--- src/main/java/fr/unilorraine/idmc/gamescatalog/core/WebSecurityConfig.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/core/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,6 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
+                //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .httpBasic();
         //@formatter:on
     }
