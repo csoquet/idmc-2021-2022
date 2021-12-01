cb7004f75d2fba9a428a42bd6233dac490f87e46
Chloé <chloe.soquet@hotmail.com>
2021-12-01 16:04:48 +0100

    BDD h2

--- src/main/java/fr/unilorraine/idmc/gamescatalog/config/WebSecurityConfig.java
+++ src/main/java/fr/unilorraine/idmc/gamescatalog/config/WebSecurityConfig.java
@@ -1,43 +1,36 @@
 package fr.unilorraine.idmc.gamescatalog.config;
 
-import org.springframework.context.annotation.Bean;
+import fr.unilorraine.idmc.gamescatalog.core.DBAuthProvider;
+import fr.unilorraine.idmc.gamescatalog.core.MD5Checker;
+import fr.unilorraine.idmc.gamescatalog.core.UserDetailsServiceFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+import static fr.unilorraine.idmc.gamescatalog.config.UsersStorage.DB;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.OPTIONS;
 
 // https://spring.io/guides/gs/securing-web/
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final SecurityProperties        properties;
+    private final UserDetailsServiceFactory detailsFactory;
+
     @Override
-    //noformat
     protected void configure(HttpSecurity http) throws Exception {
         // https://stackoverflow.com/a/14020466/1107450
         // jetbrains://idea/settings?name=Editor--Code+Style
         // Enable format markers
-        //@formatter:off
         http
                 .csrf().disable()
-//        https://developer.okta.com/docs/guides/protect-your-api/springboot/configure-cors/
-                .cors()
-                .and()
-
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/actuator/info").permitAll()
@@ -46,12 +39,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1.yml").permitAll()
                 .antMatchers("/webjars/**").permitAll()
                 .anyRequest()
-                .authenticated()
-                .and()
-//                https://stackoverflow.com/a/65132979/1107450
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .httpBasic();
-        //@formatter:on
+                .authenticated();
+
+        // https://dzone.com/articles/spring-security-authentication
+        if (properties.getStorage() == DB) {
+            final var authProvider = new DBAuthProvider(detailsFactory.inDb(), new MD5Checker());
+            http.authenticationProvider(authProvider);
+        } else {
+            http.userDetailsService(detailsFactory.inMemory());
+        }
+
+        http.httpBasic();
     }
 
     @Override
@@ -59,35 +57,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring()
                 .antMatchers(OPTIONS, "/**")
                 .antMatchers(GET, "/favicon.ico");
-    }
-
-//    https://spring.io/guides/gs/rest-service-cors/
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
-    }
-
-    @Bean
-    @Override
-    // https://docs.spring.io/spring-security/site/docs/4.2.15.RELEASE/apidocs/org/springframework/security/core/userdetails/User.html#withDefaultPasswordEncoder--
-    public UserDetailsService userDetailsService() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-        UserDetails user = User.withUsername("user")
-                .password(encoder.encode("password"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("password"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
     }
 }
